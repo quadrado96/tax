06397ba5fe0c44756dbfedc6e6e40985af95c66b
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-15 11:40:25 -0300

    classe Company e seus metodos

--- entities/Company.java
+++ entities/Company.java
@@ -1,5 +1,33 @@
 package entities;
 
-public class Company {
+public class Company extends TaxPayer {
+
+	private Integer employeeNumber;
+	
+	public Company() {
+		super();
+	}
+	
+	public Company(String name, Double anualIncome, Integer employeeNumber) {
+		super(name, anualIncome);
+		this.employeeNumber = employeeNumber;
+	}
+
+	public Integer getEmployeeNumber() {
+		return employeeNumber;
+	}
+	
+	@Override
+	public double tax() {
+		double taxPercentage;
+		if (getEmployeeNumber() > 10) {
+			taxPercentage = 0.14;
+		} else {
+			taxPercentage = 0.16;
+		}
+		return getAnualIncome() * taxPercentage;
+	}
+	
+	
 
 }
