29a3d8d34c142461f1a567d38c607c15e7e0514b
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-15 11:35:53 -0300

    classe Individual e seus metodos

--- entities/Individual.java
+++ entities/Individual.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class Individual {
+public class Individual extends TaxPayer {
+
+	private Double healthExpenditures;
+	
+	public Individual() {
+		super();
+	}
+	
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	@Override
+	public double tax() {
+		double taxPercentage = 0;
+		if (getAnualIncome() < 20000) {
+			taxPercentage = 0.15;
+		}
+		if (getAnualIncome() >= 20000) {
+			taxPercentage = 0.25;
+		}
+		if (getHealthExpenditures() > 0) {
+			healthExpenditures = healthExpenditures / 2;
+		}
+		return (getAnualIncome() * taxPercentage) - healthExpenditures;
+	}
+	
+	
 
 }
